78e7b9206dd6a9d2e5af856416000fde4f718a27
dika121 <32129307+dika121@users.noreply.github.com>
2017-10-03 01:54:23 +0700

    Update bot_3.js

--- src/bot_3.js
+++ src/bot_3.js
@@ -2,11 +2,11 @@ const LineConnect = require('./connect');
 let LINE = require('./main_3.js');
 
 const auth = {
-	authToken: ' token disini',
-	certificate: 'cert here',
+	authToken: 'Eloh4K4zokpQEKo5GP25.rb40y3Su137/1Dpq+Qh/Pq.ajC72mldGPMhfoGD2rMT169FwWXSugF8YXNa0zY7EQc=',
+	certificate: '3ebfcbd976548b9d6856e5ccc8bc06c44526650b4807a5aa9052d8ad10c99cc8',
 }
-// let client =  new LineConnect(auth);
-let client =  new LineConnect();
+let client =  new LineConnect(auth);
+// let client =  new LineConnect();
 
 client.startx().then(async (res) => {
 	
