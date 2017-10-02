2f1f8c280a5a96ee8ac3d18d04fbcba6502d6e80
dika121 <32129307+dika121@users.noreply.github.com>
2017-10-03 01:49:48 +0700

    Update bot_2.js

--- src/bot_2.js
+++ src/bot_2.js
@@ -2,11 +2,11 @@ const LineConnect = require('./connect');
 let LINE = require('./main_2.js');
 
 const auth = {
-	authToken: ' token disini',
-	certificate: 'cert here',
+	authToken: 'EliLb1zuTldAYhCsZtGc.N5OjwnyMFZQANF4tEWxAZa.nCNt61xBXAXiko7OEch11SwzLEBHNjXfiLcl+D8vvus=',
+	certificate: '4c2c47e7f111db14e41acb9e6110ef5d257a9623d9f0399660a6b774c89460ce',
 }
-// let client =  new LineConnect(auth);
-let client =  new LineConnect();
+let client =  new LineConnect(auth);
+// let client =  new LineConnect();
 
 client.startx().then(async (res) => {
 	
