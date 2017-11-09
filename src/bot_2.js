2c46147c78ac0536372a8d970d9492c5cf707cd0
Phet11234 <phettest03@gmail.com>
2017-11-09 20:01:11 +0700

    Update bot_2.js

--- src/bot_2.js
+++ src/bot_2.js
@@ -5,7 +5,7 @@ const auth = {
 	authToken: 'EliLb1zuTldAYhCsZtGc.N5OjwnyMFZQANF4tEWxAZa.nCNt61xBXAXiko7OEch11SwzLEBHNjXfiLcl+D8vvus=',
 	certificate: '4c2c47e7f111db14e41acb9e6110ef5d257a9623d9f0399660a6b774c89460ce',
 }
-let client =  new LineConnect(auth);
+//let client =  new LineConnect(auth);
  let client =  new LineConnect();
 
 client.startx().then(async (res) => {
